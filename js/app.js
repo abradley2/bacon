799f08db0cffe2f502a355637148e5c1584100a6
Tony Bradley <antbradley91@gmail.com>
2016-08-11 21:50:16 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var $ = require('jquery'),
+    _ = require('underscore'),
+    Backbone = require('backbone'),
+    Bacon = require('baconjs'),
+    ViewMediator = require('backbone-view-mediator')
+
+$(document).ready(function () {
+
+})
